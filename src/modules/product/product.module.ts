67ee0e112173a8ee5f71b79f7dda42961535c1ed
kellerkatlin <kellerkatlin.k@gmail.com>
2025-06-09 17:50:19 -0500

    feat: Add ProductItemModule and StockModule to ProductModule imports

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -4,10 +4,18 @@ import { ProductService } from './product.service';
 import { ProductImageModule } from './product-image/product-image.module';
 import { ProductVariantModule } from './product-variant/product-variant.module';
 import { ProductAttributeModule } from './product-attribute/product-attribute.module';
+import { ProductItemModule } from './product-item/product-item.module';
+import { StockModule } from 'src/common/services/stock/stock.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
-  imports: [ProductImageModule, ProductVariantModule, ProductAttributeModule],
+  imports: [
+    ProductImageModule,
+    ProductVariantModule,
+    ProductAttributeModule,
+    ProductItemModule,
+    StockModule,
+  ],
 })
 export class ProductModule {}
